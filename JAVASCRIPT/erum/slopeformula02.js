b5b7ead4f31b6b3ea46c8bfa32acc66bed26d3ce
Erum-Abdul-Latif <87967100+Erum-Abdul-Latif@users.noreply.github.com>
2022-06-22 18:52:02 +0500

    Create slopeformula02.js

---
+++ JAVASCRIPT/erum/slopeformula02.js
@@ -0,0 +1,17 @@
+let a = 10;
+let b = 2;
+
+function square(d, e) {
+  let c = Math.pow(d + e, 2);
+
+  return c;
+}
+
+console.log(square(a, b));
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
