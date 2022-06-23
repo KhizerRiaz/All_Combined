1b1248c8742fbfb75961fdeff4d3bf615584f539
Haris Aqeel <63864514+Haris-sp4rk@users.noreply.github.com>
2022-06-23 19:18:37 +0500

    Revert "last update"

--- JAVASCRIPT/erum/laplace.js
+++ JAVASCRIPT/erum/laplace.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var f=function(x,y){
 	return x*x+2*x*y;
 }
@@ -39,6 +40,48 @@ var partialDerivative=function(f,x,y,flag){
 }
 
 // alert(partialDerivative(g,Math.PI,1,"x"));
+=======
+var f = function (x, y) {
+    return x * x + 2 * x * y;
+};
+var partialDerivative = function (f, x, y, flag) {
+    var h = (dx = dy = 0.0001);
+    var dz;
+    switch (flag) {
+        case "x":
+            dz = f(x + h, y) - f(x, y);
+
+            return dz / dx;
+            break;
+        case "y":
+            dz = f(x, y + h) - f(x, y);
+            return dz / dy;
+            break;
+    }
+};
+alert(partialDerivative(f, 2, 1, "x"));
+//for sine and cosine functions
+var g = function (x, y) {
+    return Math.sin(x * y) - x * y * y;
+};
+var partialDerivative = function (f, x, y, flag) {
+    var h = (dx = dy = 0.0001);
+    var dz;
+    switch (flag) {
+        case "x":
+            dz = g(x + h, y) - g(x, y);
+
+            return dz / dx;
+            break;
+        case "y":
+            dz = g(x, y + h) - g(x, y);
+            return dz / dy;
+            break;
+    }
+};
+
+alert(partialDerivative(g, Math.PI, 1, "x"));
+>>>>>>> b6d6148740d94d4063dce8d0399682a9ea55e255
 
 //for power functions
 var p = function (x, y) {
@@ -51,9 +94,19 @@ var partialDerivative = function (f, x, y, flag) {
         case "x":
             dz = p(x + h, y) - p(x, y);
 
+<<<<<<< HEAD
 // alert(partialDerivative(p,1,4,"x"));
 
 
+=======
+            return dz / dx;
+            break;
+        case "y":
+            dz = p(x, y + h) - p(x, y);
+            return dz / dy;
+            break;
     }
+};
 
-}
+alert(partialDerivative(p, 1, 4, "x"));
+>>>>>>> b6d6148740d94d4063dce8d0399682a9ea55e255
--- JAVASCRIPT/haris/taylorExpansion.js
+++ JAVASCRIPT/haris/taylorExpansion.js
@@ -8,17 +8,7 @@ function taylorExpansion(x,numOfTerms){
     }
 
     sum = sum+1;
-    console.log(sum)
-    ex = parseInt(x)
-    num = parseInt(num)
-    if(ex.value != '' && num.value !=''){
-        document.querySelector(".output-div").textContent = sum;
-        document.querySelector(".error-message").textContent=""
-    }
-    else{
-        document.querySelector(".output-div").textContent = "";
-        document.querySelector(".error-message").textContent = "Enter the values of x and num to compute taylor series"
-    }
+    return sum
     
 
 }
