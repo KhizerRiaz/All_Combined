d4b2c0d4a142a329d8ecce9c66d23817fffa5a38
Osama <vactum0@gmail.com>
2022-06-23 17:52:52 +0500

    update unit test

--- JAVASCRIPT/neha_unit_tests/functions.test.js
+++ JAVASCRIPT/neha_unit_tests/functions.test.js
@@ -2,33 +2,33 @@ const functions = require('./functions')
 
     // distance(zain)
 
-// test ('test1:distance',()=>{
-//     expect(functions.distance(1,2,3,4)).toEqual("2.8284");
-// });
-// test ('test2:distance',()=>{
-//     expect(functions.distance(1,2,-3,-4)).toEqual("7.2111");
-// });
-// test ('test3:distance',()=>{
-//     expect(functions.distance(1,'w',-3,-4)).toEqual("NaN");
-// });
-// test ('test4:distance',()=>{
-//     expect(functions.distance(0,1.99999,5.88999,100)).toEqual("98.1769");
-// });
+test ('test1:distance',()=>{
+    expect(functions.distance(1,2,3,4)).toEqual("2.8284");
+});
+test ('test2:distance',()=>{
+    expect(functions.distance(1,2,-3,-4)).toEqual("7.2111");
+});
+test ('test3:distance',()=>{
+    expect(functions.distance(1,'w',-3,-4)).toEqual("NaN");
+});
+test ('test4:distance',()=>{
+    expect(functions.distance(0,1.99999,5.88999,100)).toEqual("98.1769");
+});
 
 // prime_number(taha)
 
-// test ('test1:prime_number',()=>{
-//     expect(functions.prime_number(11)).toBe("Is a prime number");
-// });
-// test ('test2:prime_number',()=>{
-//     expect(functions.prime_number(10)).toEqual("Is not a prime number");
-// });
-// test ('test3:prime_number',()=>{
-//     expect(functions.prime_number('a')).toEqual("Wrong input type");
-// });
-// test ('test4:prime_number',()=>{
-//     expect(functions.prime_number(-4)).toEqual("The number is not a prime number.");
-// });
+test ('test1:prime_number',()=>{
+    expect(functions.prime_number(11)).toBe("Is a prime number");
+});
+test ('test2:prime_number',()=>{
+    expect(functions.prime_number(10)).toEqual("Is not a prime number");
+});
+test ('test3:prime_number',()=>{
+    expect(functions.prime_number('a')).toEqual("Wrong input type");
+});
+test ('test4:prime_number',()=>{
+    expect(functions.prime_number(-4)).toEqual("The number is not a prime number.");
+});
 
 
 // sum(mohsin)
