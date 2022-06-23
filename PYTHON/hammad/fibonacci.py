9c7ffae271e7dd6a169405ea716d9ddfe5246561
chander-kumar <chanderharwani88@gmail.com>
2022-06-23 17:27:40 +0500

    Fibonacci and Relativity Testing Complete

--- PYTHON/hammad/fibonacci.py
+++ PYTHON/hammad/fibonacci.py
@@ -1,17 +1,27 @@
-def fib(a):
-    if(a<=1):
-        return a
-    else:
-        return(fib(a-1)+fib(a-2))
-
-
-def getNum():
+def fibonacciSeries(a):
     try:
-        n=int(input("Enter number"))
-        if n>0:
-            return n
+        if(a<=1):
+            return a
         else:
-            return 0
+            return(fibonacciSeries(a-1)+fibonacciSeries(a-2))
     except:
-        return 0
-print(fib(getNum()))
+        return "Enter Interger Number"
+
+# def fib(a):
+#     if(a<=1):
+#         return a
+#     else:
+#         return(fib(a-1)+fib(a-2))
+
+
+# def getNum():
+#     try:
+#         n=int(input("Enter number"))
+#         if n>0:
+#             return n
+#         else:
+#             return 0
+#     except:
+#         return 0
+# print(fib(getNum()))
+
